e2a0447b159873f2561101c550dbe1fbd063bff7
Satyam Sovan Mishra <33446554+satyamsovan123@users.noreply.github.com>
2024-08-14 18:15:18 +0530

    Complete forgot password

--- app/controllers/authentication/forgotPassword.js
+++ app/controllers/authentication/forgotPassword.js
@@ -2,6 +2,28 @@ const User = require("../../models/User");
 
 const forgotPassword = async (req, res) => {
   try {
+    if (!req.body.email) {
+      return res.status(400).send({ message: "Email is required." });
+    }
+
+    if (
+      !req.body.password ||
+      !req.body.confirmPassword ||
+      req.body.password !== req.body.confirmPassword
+    ) {
+      return res.status(400).send({
+        message:
+          "Password and confirm password should be same and are required.",
+      });
+    }
+
+    req.body.password = await encryptPassword(req.body.password);
+
+    await User.findOneAndUpdate(
+      { email: req.body.email },
+      { password: req.body.password }
+    );
+
     return res.status(200).send({ message: "Password reset was successful." });
   } catch (error) {
     console.error(error);
